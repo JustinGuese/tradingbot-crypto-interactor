d13a68014ab6f02c923d385e12c508b5c6842c7a
Justin Guese <guese.justin@gmail.com>
2022-04-04 13:18:40 +0200

    routing error

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="tradingbotinteractor",
-    version="0.0.4",
+    version="0.0.5",
     author="Justin Guese",
     author_email="guese.justin@gmail.com",
     description="Interacts with accounthandler",
